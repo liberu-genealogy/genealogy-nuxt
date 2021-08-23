1b27fdb3f8d21d65b489a9cb26b7284131ff82d3
4-l-clover <crsun.dev@hotmail.com>
2021-08-23 15:54:25 +0530

    Add store and routing

---
+++ pages/403.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container p-4 has-text-centered">
+        <span class="icon is-large">
+            <fa class="has-text-danger"
+                icon="ban"
+                size="2x"/>
+        </span>
+        <p class="subtitle is-3">
+            {{ this.$i18n('You are not authorized here') }}
+        </p>
+    </div>
+</template>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBan } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBan);
+
+export default {
+    name: 'unauthorized',
+    head: {
+        title: 'Unauthorized'
+    }
+};
+</script>
+
---
+++ pages/system.vue
@@ -0,0 +1,13 @@
+<template>
+</template>
+
+<script>
+export default {
+    key(route) {
+        return route.path
+    },
+    meta: {
+        breadcrumb: 'system'
+    }
+};
+</script>
