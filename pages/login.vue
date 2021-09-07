08fc6292fd63a8d263c26716c5af20f63f70e627
andriferry <ferryandri35@gmail.com>
2021-09-07 17:35:45 +0700

    fix conflict

--- pages/login.vue
+++ pages/login.vue
@@ -36,15 +36,7 @@
       title: "Login",
     },
 
-<<<<<<< HEAD
     components: { AuthForm, Email, Password, Remember, LoginRegister },
-=======
-    head: {
-        title: "Login"
-    },
-
-    components: { AuthForm, Email, Password, Remember, Submit },
->>>>>>> rebase
 
     data: () => ({
       errors: new Errors(),
@@ -71,27 +63,11 @@
           this.setCsrfToken({ token: data.csrfToken, $axios: this.$axios });
         }
 
-<<<<<<< HEAD
         setTimeout(() => {
           this.login();
           this.home(true);
         }, 500);
       },
-=======
-            setTimeout(() => {
-                this.login();
-                this.home(true);
-                this.$router.push("/dashboard");
-            }, 500);
-        },
-
-        socialLogin(service) {
-            this.$axios.get(`api/login/${service}`).then(response=>{
-                // console.log(response.data);
-                window.location.href = response.data;
-            })
-        }
->>>>>>> rebase
     },
   };
 </script>
