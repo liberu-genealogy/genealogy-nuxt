62cc762ee6127958609e666e61dc8672673c33f3
Curtis Delicata <curtis.delicata@outlook.com>
2021-09-20 04:33:25 +0100

    Update Index.vue

--- pages/gedcom/Index.vue
+++ pages/gedcom/Index.vue
@@ -1,115 +1,184 @@
 <template>
     <div>
-        <div>
-            <input
-                type="text"
-                style="display:none"
-                :value.prop="slug">
-            <input
-                type="file"
-                style="display:none"
-                @change="selectedFile"
-                ref="fileInput"
-                accept="GED">
-            <button @click="$refs.fileInput.click()">
-Select Gedcom File
-</button>
-            <button v-if="file" @click="uploadFile">
-Upload
-</button>
-        </div>
-        <div v-if="uploadPercentage">
-            <span class="ml-1">Upload</span>
-            <progress
-                max="100"
-                :value.prop="uploadPercentage"/>
-            <span :value.prop="total">{{ uploadPercentage }}%</span>
-        </div>
-        <div v-if="total">
-            <span class="ml-1">Import</span>
-            <progress
-                :max.prop="total"
-                :value.prop="complete"/>
-            <span :value.prop="total">{{ complete }}/{{ total }}</span>
-        </div>
+        <loading :active.sync="isLoading" :color="color" :background-color="backgroundColor"> </loading>
+            <div class="columns is-gapless is-multiline is-mobile">
+                <div class="column is-12">
+                    <h1 class="is-size-4 has-text-black">
+                        <span class="has-text-weight-medium">Gedcoms</span>
+                    </h1>
+                </div>
+                <div class="column is-12">
+                    <nav class="breadcrumb mt-1 mb-0" aria-label="breadcrumbs">
+                        <ul>
+                            <li><a class="is-size-7 has-text-weight-medium has-text-link"
+                                    href="/dashboard">Home</a></li>
+                            <li class="is-size-7 has-text-weight-medium is-active"><a href="/dashboard"
+                                    aria-current="page">Gedcoms</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <form @submit.prevent="submit()" enctype="multipart/form-data">
+                <div class="columns is-variable is-3 is-desktop is-flex-desktop-only ai--s">
+                    <div class="column is-12">
+
+                        <div class="card has-background-white has-text-black" style="height: 500px;">
+                            <div class="card-content">
+                                <div v-if="response" class="notification is-success">
+                                    {{ response }}
+                                </div>
+                                <div v-if="error" class="notification is-danger">
+                                    {{ message }}
+                                </div>
+                                <div v-for="error in errors" class="notification is-danger">
+                                    {{ error[0] }}
+                                </div>
+                                <input type="hidden" v-model="fileName" v-if="!isLoading">
+                                <div class="field import_block" v-if="!isLoading">
+                                    <div class="file is-large is-boxed has-background-primary">
+                                        <label class="file-label">
+
+                                            <input class="file-input" type="file" @change="handleSelectedFiles" name="file" ref="fileInput" accept=".ged">
+                                            <span class="file-cta">
+                                                <span class="file-label">
+                                                    <span class="file-icon">
+                                                        <font-awesome-icon :icon="['fas', 'upload']"/>
+                                                    </span>
+                                                    Select GEDCOM File
+                                                </span>
+                                            </span>
+                                        </label>
+
+                                    </div>
+                                    <p class="help"  :class="{ 'is-danger': $v.fileName.$error }" v-if="!$v.fileName.required">Field is required</p>
+                                </div>
+
+                                <b-progress
+                                  v-if="isLoading"
+                                  type="is-success"
+                                  :max="total"
+                                  :value="complete"
+                                  show-value
+                                />
+
+                                <button type="submit"
+                                class="button theme-button theme-button-xl has-background-primary is-uppercase has-text-weight-medium has-text-white">
+                                Submit
+                            </button>
+                                <ul class="bullet_list mt-5">
+                                    <li>We value your privacy and do not share your GEDCOM data with any third parties. Your GEDCOM file is used by the website only for processing your data and importing into your tree that you have selected.</li>
+                                    <li>One tree is free for life for everyone and if you require more you will need to subscribe which helps us continue to provide services and develop new features.</li>
+                                    <li>Please select a GEDCOM file exported from your desktop software or online website and click upload. It should begin to process and show imported data processing. If it stops before fully importing contact <a href="mailto:support@familytree365.com" class="has-text-link">support@familytree365.com</a></li>
+                                    <li>We do not use your GEDCOM file for any other purpose.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
     </div>
 </template>
+f
 <script>
-import Pusher from 'pusher-js'; // import Pusher
+import { mapGetters } from 'vuex'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
+    layout: 'auth',
 
-    data() {
-        return {
-            file: null,
-            uploadPercentage: 0,
-            total: 0,
-            complete: 0,
-            slug: null,
-        };
+    components: {
+      Loading
+    },
+
+    middleware: 'permission',
+
+    meta: {
+      permission: { name: 'gedcom import menu' }
+    },
+
+    data: () => ({
+      error: false,
+      message: "",
+      errors:null,
+      file: undefined,
+      fileName: '',
+      isLoading: false,
+      fullPage: true,
+      color: '#4fcf8d',
+      backgroundColor: '#ffffff',
+      response : null,
+      total: 100,
+      complete: 0,
+    }),
+
+    validations: {
+      fileName: {
+        required,
+      },
     },
 
     computed: {
-        uploadLink() {
-            return '/api/gedcom/store';
-        },
-        getProgressLink() {
-            return '/api/gedcom/progress';
-        },
+       ...mapGetters([
+              'isAuthenticated',
+              'loggedInUser','loggedInUser']),
     },
-    created() {
-        this.subscribe();
-        this.getProgressData();
-    },
-    methods: {
-        subscribe() {
-            const pusher = new Pusher(this.PUSHER_KEY, { cluster: this.PUSHER_CLUSTER });
-            pusher.subscribe('gedcom-progress');
-            pusher.bind('newMessage', data => {
-                const { slug, total, complete } = data;
-                if (slug === this.slug) {
-                    this.slug = slug;
-                    this.total = total;
-                    this.complete = complete;
-                }
-            });
-        },
-        selectedFile(event) {
-            [this.file] = event.target.files;
-            this.uploadPercentage = 0;
-            this.total = 0;
-            this.slug = Math.random().toString(36).substring(2, 15)
-             + Math.random().toString(36).substring(2, 15);
-        },
-        uploadFile() {
-            const fd = new FormData();
-            fd.append('slug', this.slug);
-            fd.append('file', this.file, 'file.ged');
-            axios
-                .post(this.uploadLink, fd, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                    },
-                    onUploadProgress: function (progressEvent) {
-                        const pe = Math.round((progressEvent.loaded / progressEvent.total) * 100);
-                        this.uploadPercentage = parseInt(pe, 10);
-                    }.bind(this),
-                })
-                .then()
-                .catch();
-        },
-        // getProgressData() {
-        //     const params = {};
-        //     axios
-        //         .get(this.getProgressLink, { params })
-        //         .then(res => {
-        //             const data = JSON.parse(res);
-        //             this.slug = data.slug;
-        //         })
-        //         .catch();
-        // },
-    },
-};
-</script>
 
-<style lang="scss" scoped></style>
+    mounted() {
+      this.subscribeToUploadProgress()
+    },
+
+    methods: {
+      handleSelectedFiles(event) {
+        console.log(this.$refs.fileInput.files[0])
+        this.file = this.$refs.fileInput.files[0]
+        this.fileName = this.file.name
+      },
+
+      async submit() {
+        this.$v.$touch();
+
+        if (this.$v.$invalid) {
+          return -1
+        }
+
+        this.total = 100
+        this.complete = 0
+
+        this.isLoading = true
+        let formData = new FormData()
+        formData.append('file',  this.file)
+
+        try {
+          const response = await this.$axios
+            .$post("/api/gedcom", formData, {
+                headers: {
+                  'content-type': 'multipart/form-data',
+                  'Access-Control-Allow-Origin': '*'
+                }
+            })
+
+          this.isLoading = false
+          this.response = response[0]
+        } catch (error) {
+          this.error = true
+          this.message = error.response.data.message
+          this.errors =  error.response.data.errors
+        }
+      },
+
+      subscribeToUploadProgress() {
+        this.$echo.channel(`user.${this.loggedInUser.id}`)
+          .listen('.gedcomProgress', message => {
+            this.total = message.total
+            this.complete = message.complete
+          })
+      }
+      }
+    }
+
+</script>
+<style scoped>
+    @import '~/assets/css/admin.css';
+</style>
