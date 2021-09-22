64ab4fce2a39d8721e631ecbc7c790445d786e26
Curtis Delicata <curtis.delicata@outlook.com>
2021-09-22 09:14:08 +0100

    Update index.vue

--- pages/authors/index.vue
+++ pages/authors/index.vue
@@ -2,7 +2,11 @@
     <enso-table class="box is-paddingless raises-on-hover"
         id="authors"/>
 </template>
-
+<router>
+{
+    name: 'authors.index'
+}
+</router>
 <script>
 import { EnsoTable } from '@enso-ui/tables/bulma';
 
