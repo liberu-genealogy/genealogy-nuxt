7523913530b3cd9323433f056f4071906aed7ddc
Curtis Delicata <curtis.delicata@outlook.com>
2021-09-22 09:11:25 +0100

    Update create.vue

--- pages/authors/create.vue
+++ pages/authors/create.vue
@@ -10,7 +10,11 @@
 import { EnsoForm } from '@enso-ui/forms/bulma';
 
 export default {
-    name: 'Create',
+layout: 'auth',
+    meta: {
+        permission: { name: 'authors menu' },
+        title: 'Authors - Create'
+    },
 
     components: { EnsoForm },
 };
