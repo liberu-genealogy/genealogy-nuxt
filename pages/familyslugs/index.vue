de1c9c6c778ac8a204ebff15d7e41778d0cfcf13
Curtis Delicata <curtis.delicata@outlook.com>
2021-09-23 12:40:58 +0100

    Update index.vue

--- pages/familyslugs/index.vue
+++ pages/familyslugs/index.vue
@@ -7,7 +7,13 @@
 import { EnsoTable } from '@enso-ui/tables/bulma';
 
 export default {
-    name: 'Index',
+
+layout: 'auth',
+    meta: {
+        permission: { name: 'familyslugs menu' },
+        title: 'Family Slugs - Index'
+    },
+
 
     components: { EnsoTable },
 };
