d4c5892fbae33fa58d6b0c8586ce5ac7ebfbbc90
Curtis Delicata <curtis.delicata@outlook.com>
2021-09-23 13:35:22 +0100

    Update index.vue

--- pages/personlds/index.vue
+++ pages/personlds/index.vue
@@ -2,12 +2,21 @@
     <enso-table class="box is-paddingless raises-on-hover"
         id="person_lds"/>
 </template>
-
+<router>
+{
+    name: 'personlds.index'
+}
+</router>
 <script>
 import { EnsoTable } from '@enso-ui/tables/bulma';
 
 export default {
-    name: 'Index',
+
+layout: 'auth',
+    meta: {
+        permission: { name: 'personlds menu' },
+        title: 'Person LDS - Index'
+    },
 
     components: { EnsoTable },
 };
