1558849a177dda4f00c53b9e836cec62242d9135
Curtis Delicata <curtis.delicata@outlook.com>
2021-09-23 14:30:52 +0100

    Update edit.vue

--- pages/personsubm/edit.vue
+++ pages/personsubm/edit.vue
@@ -6,11 +6,21 @@
     </div>
 </template>
 
+<router>
+{
+    name: 'personsubm.edit'
+}
+</router>
 <script>
 import { EnsoForm } from '@enso-ui/forms/bulma';
 
 export default {
-    name: 'Edit',
+
+layout: 'auth',
+    meta: {
+        permission: { name: 'personsubm menu' },
+        title: 'Person Subm - Edit'
+    },
 
     components: { EnsoForm },
 };
