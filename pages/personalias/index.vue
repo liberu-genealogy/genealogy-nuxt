674addbd32c41b54f1b7b14ef9d8cb79332bbb9d
Curtis Delicata <curtis.delicata@outlook.com>
2021-09-23 13:07:49 +0100

    Update index.vue

--- pages/personalias/index.vue
+++ pages/personalias/index.vue
@@ -3,11 +3,21 @@
         id="person_alias"/>
 </template>
 
+<router>
+{
+    name: 'personalias.index'
+}
+</router>
 <script>
 import { EnsoTable } from '@enso-ui/tables/bulma';
 
 export default {
-    name: 'Index',
+
+layout: 'auth',
+    meta: {
+        permission: { name: 'personalias menu' },
+        title: 'Person Alias - Index'
+    },
 
     components: { EnsoTable },
 };
