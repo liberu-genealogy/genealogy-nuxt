daf3d8cfd2cae97c5bccd7ffe8c80a686b196301
Curtis Delicata <curtis.delicata@outlook.com>
2021-09-23 12:40:06 +0100

    Update create.vue

--- pages/familyslugs/create.vue
+++ pages/familyslugs/create.vue
@@ -10,7 +10,11 @@
 import { EnsoForm } from '@enso-ui/forms/bulma';
 
 export default {
-    name: 'Create',
+layout: 'auth',
+    meta: {
+        permission: { name: 'familyslugs menu' },
+        title: 'Family Slugs - Create'
+    },
 
     components: { EnsoForm },
 };
