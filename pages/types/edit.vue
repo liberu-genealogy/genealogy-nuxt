49f04294745f16709210d7449dac772d12e0765a
truth4you <86637147+truth4you@users.noreply.github.com>
2021-09-24 03:40:26 -0700

    types update

--- pages/types/edit.vue
+++ pages/types/edit.vue
@@ -5,13 +5,21 @@
         </div>
     </div>
 </template>
-
+<router>
+{
+    name: 'types.edit'
+}
+</router>
 <script>
 import { EnsoForm } from '@enso-ui/forms/bulma';
 
 export default {
     name: 'Edit',
-
+    layout: 'auth',
+    meta: {
+        permission: { name: 'types menu' },
+        title: 'Types - Edit'
+    },
     components: { EnsoForm },
 };
 </script>
--- pages/types/show.vue
+++ pages/types/show.vue
@@ -1,10 +1,19 @@
 <template>
     <div/>
 </template>
-
+<router>
+{
+    name: 'types.show'
+}
+</router>
 <script>
 export default {
     name: 'Show',
+    layout: 'auth',
+    meta: {
+        permission: { name: 'types menu' },
+        title: 'Types - Show'
+    },
 };
 </script>
 
