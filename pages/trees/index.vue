6d1e6501b72ddb2356d91323bb07163fa6658462
shahghasiadil <shaghasy555@gmail.com>
2021-11-10 20:01:57 +0430

    pedigree chart fix

---
+++ pages/trees/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <enso-table class="box is-paddingless raises-on-hover" id="notes" />
+</template>
+
+<router>
+{
+    name: 'trees.show'
+}
+</router>
+<script>
+import { EnsoTable } from "@enso-ui/tables/bulma";
+
+export default {
+  layout: "auth",
+  meta: {
+    permission: { name: "trees menu" },
+    title: "Trees - Index",
+  },
+
+  components: { EnsoTable },
+};
+</script>
+
+<style lang="scss">
+</style>
