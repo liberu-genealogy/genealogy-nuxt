6282480ae88a97b9063e3d164681e6a951da9157
Curtis Delicata <curtis.delicata@outlook.com>
2022-07-30 13:45:06 +0000

    Git component for tasks

--- pages/tasks/index.vue
+++ pages/tasks/index.vue
@@ -12,7 +12,7 @@
             @reset="$refs.filterState.reset()"
             id="tasks"
             ref="table">
-            <template v-slot:name="{ row }">
+            <template #name="{ row }">
                 <span>
                     {{ row.name }}
                 </span>
@@ -22,8 +22,8 @@
                         size="xs"/>
                 </span>
             </template>
-            <template v-slot:flag="{ row, column }">
-                <v-popover trigger="click"
+            <template #flag="{ row, column }">
+                <dropdown :triggers="['click']"
                     :ref="`flag-${row.id}`">
                     <span class="icon is-clickable"
                         :class="`has-text-${column.enum._get(row.flag).toLowerCase()}`"
@@ -35,18 +35,17 @@
                         <fa icon="cog"
                             size="xs"/>
                     </span>
-                    <template v-slot:popover>
+                    <template #popper>
                         <flags v-model="row.flag"
-                            @input="
+                            @update:model-value="
                                 update(row.id, 'flag', row.flag);
                                 $refs[`flag-${row.id}`].hide()
-                            "
-                            v-if="isOpen(`flag-${row.id}`)"/>
+                            "/>
                     </template>
-                </v-popover>
+                </dropdown>
             </template>
-            <template v-slot:reminder="{ row }">
-                <v-popover trigger="click"
+            <template #reminder="{ row }">
+                <dropdown :triggers="['click']"
                     :auto-hide="false"
                     :ref="`reminder-${row.id}`">
                     <span class="icon is-clickable"
@@ -60,7 +59,7 @@
                         <fa icon="cog"
                             size="xs"/>
                     </span>
-                    <template v-slot:popover>
+                    <template #popper>
                         <enso-datepicker class="reminder-picker"
                             v-click-outside="() => {
                                 update(row.id, 'reminder', row.rawReminder);
@@ -69,20 +68,18 @@
                             v-model="row.rawReminder"
                             format="Y-m-d H:i:s"
                             time
-                            :alt-format="dateFormat"
-                            v-if="isOpen(`reminder-${row.id}`)"/>
+                            :alt-format="dateFormat"/>
                     </template>
-                </v-popover>
+                </dropdown>
             </template>
-            <template v-slot:allocatedTo="{ row }">
-                <v-popover trigger="click"
+            <template #allocatedTo="{ row }">
+                <dropdown :triggers="['click']"
                     :ref="`allocated_to-${row.id}`"
                     v-if="canChangeAllocation">
                     <avatar class="is-24x24 is-clickable"
                         :user="row.allocatedTo"/>
-                    <template v-slot:popover>
-                        <div class="allocated-to"
-                            v-if="isOpen(`allocated_to-${row.id}`)">
+                    <template #popper>
+                        <div class="allocated-to">
                             <enso-select v-model="row.allocatedTo.id"
                                 @select="
                                     update(row.id, 'allocated_to', row.allocatedTo.id);
@@ -93,17 +90,19 @@
                                 label="person.name"/>
                         </div>
                     </template>
-                </v-popover>
+                </dropdown>
                 <avatar class="is-24x24"
                     :user="row.allocatedTo"
                     v-else/>
             </template>
-            <template v-slot:completed="{ row }">
-                <vue-switch class="is-medium"
-                    v-model="row.completed"
-                    @input="update(row.id, 'completed', row.completed)"/>
+            <template #completed="{ row }">
+                <div class="is-flex is-justify-content-center">
+                    <vue-switch class="is-medium"
+                        v-model="row.completed"
+                        @update:model-value="update(row.id, 'completed', row.completed)"/>
+                </div>
             </template>
-            <template v-slot:createdBy="{ row: { createdBy } }">
+            <template #createdBy="{ row: { createdBy } }">
                 <avatar class="is-24x24"
                     :user="createdBy"/>
             </template>
@@ -116,11 +115,6 @@
             ref="filterState"/>
     </div>
 </template>
-<!--<router>-->
-<!--{-->
-<!--    name: 'tasks.index',-->
-<!--}-->
-<!--</router>-->
 
 <script>
 import { mapState } from 'vuex';
@@ -129,25 +123,22 @@ import { EnsoTable } from '@enso-ui/tables/bulma';
 import { EnsoSelect } from '@enso-ui/select/bulma';
 import VueSwitch from '@enso-ui/switch/bulma';
 import { EnsoDatepicker } from '@enso-ui/datepicker/bulma';
-import Avatar from '~/components/users/bulma/pages/users/components/Avatar.vue';
-import { faClock, faInfoCircle, faCog } from '@fortawesome/free-solid-svg-icons';
+import { Avatar } from '@enso-ui/users';
+import { FontAwesomeIcon as Fa } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { VTooltip, VPopover } from 'v-tooltip';
+import { faClock, faInfoCircle, faCog } from '@fortawesome/free-solid-svg-icons';
+import { Dropdown } from 'v-tooltip';
 import { clickOutside } from '@enso-ui/directives';
-import Filters from '~/components/tasks/bulma/pages/tasks/components/Filters.vue';
-import Flags from '~/components/tasks/bulma/pages/tasks/components/Flags.vue';
+import Filters from '~/components/tasks/bulma/pages/tasks/Filters.vue';
+import Flags from '~/components/tasks/bulma/pages/tasks/Flags.vue';
+
 
 library.add(faClock, faInfoCircle, faCog);
 
 export default {
-    meta: {
-        breadcrumb: 'index',
-        title: 'Tasks',
-    },
+    name: 'Index',
 
-    inject: ['i18n', 'route', 'toastr', 'errorHandler'],
-
-    directives: { tooltip: VTooltip, clickOutside },
+    directives: { clickOutside },
 
     components: {
         Avatar,
@@ -155,14 +146,17 @@ export default {
         EnsoTable,
         FilterState,
         VueSwitch,
-        VPopover,
+        Dropdown,
+        Fa,
         Filters,
         Flags,
         EnsoSelect,
     },
 
+    inject: ['http', 'i18n', 'route', 'toastr', 'errorHandler'],
+
     data: () => ({
-        apiVersion: 1.1,
+        apiVersion: 2,
         ready: false,
         filters: {
             tasks: {
@@ -180,7 +174,7 @@ export default {
             },
         },
         params: {
-            dateInterval: 'today',
+            dateFilter: 'today',
             overdue: null,
         },
     }),
@@ -199,7 +193,7 @@ export default {
 
     methods: {
         update(id, attribute, value) {
-            this.$axios.patch(this.route('tasks.update', { task: id }), {
+            this.http.patch(this.route('tasks.update', { task: id }), {
                 [attribute]: value,
             }).then(({ data: { message } }) => {
                 this.toastr.success(message);
@@ -211,10 +205,6 @@ export default {
                     this.errorHandler(error);
                 }
             });
-        },
-
-        isOpen(ref) {
-            return this.$refs[ref]?.isOpen;
         },
     },
 };
