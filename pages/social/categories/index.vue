1fbe7209df21b147ba5277c1ca277e1153a02b3c
Curtis Delicata <curtis.delicata@outlook.com>
2023-02-01 10:26:21 +0000

    Update index.vue

--- pages/social/categories/index.vue
+++ pages/social/categories/index.vue
@@ -3,9 +3,9 @@
     <div class="columns is-gapless is-multiline is-mobile">
       <div class="column is-12">
 <h1><a href="https://www.facebook.com/groups/familytree365" target="_blank" class="button is-success">
-Family Tree 365 Support</a></h1>
+Family Tree 365 Support</a></h1><br />
 <h1><a href="https://www.facebook.com/groups/genealogychat" target="_blank" class="button is-success">
-Genealogy Discussion</a></h1>
+Genealogy Discussion</a></h1><br />
 <h1><a href="https://www.facebook.com/groups/dna365" target="_blank" class="button is-success">
 DNA & Genetics Discussion</a></h1>
 
