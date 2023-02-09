f224e073f502baf835591a9bfe6beeaa741a0c3b
Knight1973 <alfmorales370@gmail.com>
2023-02-09 19:53:39 +0200

    fix: Fan chart #233

--- pages/chart/show.vue
+++ pages/chart/show.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    <div id="tree"></div>
+    <loading
+        :active="isLoading"
+        :color="color"
+        :background-color="backgroundColor"
+    />
+      <div class="select-box">
+        <v-select
+            style="width: 100%"
+            v-model="selected_person"
+            :options="persons.map(item => item = {label: item.name, value: item.id})"
+            @input="fetchData"
+        />
+        <v-select
+            style="width: 100px"
+            v-model="generation"
+            :options="Array.from(Array(10).keys()).map(item => item = {label: item, value: item})"
+            @input="fetchData"
+        />
+      </div>
+    <div style="height: 700px" id="webtrees-pedigree-chart-container"></div>
   </div>
 </template>
 <router>
@@ -13,30 +32,112 @@
 <script>
 import { EnsoTable } from "@enso-ui/tables/bulma";
 import { PedigreeChart } from "/assets/js/modules/index";
+import Loading from 'vue-loading-overlay'
+import vSelect from 'vue-select'
 export default {
   layout: "auth",
   meta: {
     permission: { name: "trees menu" },
     title: "Trees - Index",
   },
-
-  components: { EnsoTable },
+  data: () => ({
+    familyData: {},
+    persons: [],
+    selected_person: {},
+    data: {},
+    isLoading: false,
+    color: '#4fcf8d',
+    backgroundColor: '#ffffff',
+    generation: { label: 1, value: 1 },
+    thumbnail_man: "/images/thumbnail-man.svg",
+    thumbnail_woman: "/images/thumbnail-woman.svg",
+  }),
+  components: { 
+    EnsoTable,
+    Loading,
+    vSelect
+  },
   methods: {
-    fetchData() {
+  checkBirthDeathDate(birth, death) {
+      if(death) {
+        if(!birth) return ""
+        return `${birth.toString().slice(0, 4)}-${death.toString().slice(0, 4)}`
+      }else {
+        if(!birth) return ""
+        return `Born ${birth.toString().slice(0, 4)}`
+      }
+    },
+
+    checkChildren(id) {
+      let parents = [];
+      let childIds = [];
+
+      this.data.persons[id].own_unions?.forEach((union) => {
+        if(this.data.unions[union]) {
+          this.data.unions[union].children.forEach((childId) => {
+            childIds.push(childId)
+          })
+        }
+      })
+      if(!childIds.length) return
+      this.data.persons[id].own_unions?.forEach((union) => {
+        this.data.unions[union]?.children.forEach((childId) => {
+          let person = this.data.persons[childId];
+          parents.push({
+            id: person.id,
+            name: person.name,
+            title: person.titl,
+            firstNames:[person.name],
+            lastNames:[''],
+            generation: person.generation,
+            sex: person.sex ? person.sex : "M",
+            xref : 1,
+            birth: person.birthday ? person.birthday : person.birth_year,
+            death: person.deathday ? person.deathday : person.death_year,
+            timespan: this.checkBirthDeathDate(person.birthday ? person.birthday : person.birth_year, person.deathday ? person.deathday : person.death_year),
+            thumbnail : person.sex ? this.thumbnail_woman : this.thumbnail_man,
+            parents: this.checkChildren(childId)
+          })
+        })
+      })
+      return parents
+    },
+
+    checkFamilyData(id = null) {
+      const person = id ? this.data.persons[id] : this.data.persons[this.data.start]
+      return {
+        id: person.id,
+        name: person.name,
+        title: person.titl,
+        firstNames:[person.name],
+        lastNames:[''],
+        generation: person.generation,
+        sex: person.sex ? person.sex : "M",
+        xref : 1,
+        birth: person.birthday ? person.birthday : person.birth_year,
+        death: person.deathday ? person.deathday : person.death_year,
+        timespan: this.checkBirthDeathDate(person.birthday ? person.birthday : person.birth_year, person.deathday ? person.deathday : person.death_year),
+        thumbnail : person.sex ? this.thumbnail_woman : this.thumbnail_man,
+        parents: this.checkChildren(this.data.start)
+      }
+    },
+    async fetchData() {
+      if(!this.selected_person.value) return;
+      let params = {start_id: this.selected_person.value, generation: this.generation.value }
       this.$axios
-        .$get("/api/trees/show")
-        // .then((res) => res.json())
-        // .then((response) => response.json())
+        .$get("/api/trees/show", { params: params })
         .then((res) => {
-          // console.log(res.data);
+          this.data = res;
+          this.familyData = this.checkFamilyData();
+          console.log(this.familyData);
           const pedigreeChart = new PedigreeChart(
-            "webtrees-pedigree-chart-container",
+            "#webtrees-pedigree-chart-container",
             {
               labels: {
                 zoom: "Use Ctrl + scroll to zoom in the view",
                 move: "Move the view with two fingers",
               },
-              generations: 4,
+              generations: 1,
               defaultColor: "#0000FF",
               fontColor: "#0000FF",
               showEmptyBoxes: true,
@@ -45,17 +146,31 @@ export default {
             }
           );
           pedigreeChart.cssFile = "assets/css/svg.css";
-          // Draw chart
-          // console.log("ff" + res);
-          pedigreeChart.draw(res);
+          pedigreeChart.draw(this.familyData);
+      });
+    },
+
+    getPersons() {
+      this.isLoading = true
+      this.$axios
+        .$get("/api/persons")
+        .then((res) => {
+          this.persons = res;
+          this.isLoading = false;
         });
     },
   },
   mounted() {
-    this.fetchData();
+    this.getPersons();
   },
 };
 </script>
 
 <style lang="scss">
+.select-box {
+  display: flex;
+  width: 100%;
+  gap: 20px;
+  justify-content: space-between;
+}
 </style>
