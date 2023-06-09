1293518d28d031a84c91252bc22805a7701463f3
phoenixdevguru <phoenixdevguru>
2023-06-09 07:16:39 -0600

    fix: authorization

--- plugins/echo.js
+++ plugins/echo.js
@@ -4,7 +4,8 @@ window.io = require('socket.io-client')
 export default ({ app, env }, inject) => {
   const echo = new Echo({
     broadcaster: 'socket.io',
-    host: `https://familytree365.com:6001`,
+    // host: `https://familytree365.com:6001`,
+    host: `http://127.0.0.1:6001`,
   })
 
   inject('echo', echo)
