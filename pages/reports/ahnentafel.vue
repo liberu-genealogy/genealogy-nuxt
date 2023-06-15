c6fa13c876e0e7c8cfc56ed097ff666b349c8426
Josh Kisb <joshuakisb@gmail.com>
2023-06-15 14:27:24 +0300

    ahnentafel report

--- pages/reports/ahnentafel.vue
+++ pages/reports/ahnentafel.vue
@@ -7,7 +7,28 @@
       <v-select style="width: 100px" v-model="generation"
         :options="Array.from(Array(10).keys()).map(item => item = { label: item, value: item })" @input="fetchData" />
     </div>
-    <div style="min-height: 700px; padding: 20px 8px;" id="webtrees-pedigree-chart-container"></div>
+    <div style="min-height: 700px; padding: 20px 8px;" id="webtrees-pedigree-chart-container">
+      <div v-if="!!familyData.id">
+        <p class="text-center">Ahnentafel Report for {{familyData.firstNames}}</p>
+        <div v-for="generation in Object.keys(reportLevels).sort()">
+          <div style="">
+            <p style="padding: 12px 0;">Generation {{reportLevels[generation].level}}</p>
+            <div v-for="p in Object.keys(reportLevels[generation].data).sort()">
+              <div style="display: flex; margin: 8px 0;">
+                <p style="min-width: 30px;">{{ reportLevels[generation].data[p].label }}.</p> 
+                <p>
+                  <strong>{{ reportLevels[generation].data[p].name }}</strong>: 
+                  born {{ reportLevels[generation].data[p].person.birthday }} 
+                  <span>in {{ reportLevels[generation].data[p].person.birthplace }};</span>
+                  died {{ reportLevels[generation].data[p].person.deathday }} 
+                  <span v-if="!!reportLevels[generation].data[p].person.deathplace">in {{ reportLevels[generation].data[p].person.deathplace }}</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <router>
@@ -23,7 +44,7 @@ import { EnsoTable } from "@enso-ui/tables/bulma";
 import Loading from 'vue-loading-overlay'
 import vSelect from 'vue-select'
 import style from '/assets/css/svg.css'
-import {createDisplayName, getBirthYear, getDeathYear } from '/utils/personHelper';
+import {createDisplayName, getBirthYear, getDeathYear, formatDateToYMD } from '/utils/personHelper';
 export default {
   layout: "auth",
   meta: {
@@ -83,7 +104,11 @@ export default {
             id: person.id,
             name: person.name,
             title: person.titl,
-            firstNames: [createDisplayName(person)],
+            birthplace: person.birthday_plac,
+            birthday: person.birth ?? formatDateToYMD(person.birthday),
+            deathday: person.death ?? formatDateToYMD(person.deathday),
+            deathplace: person.deathday_plac,
+            firstNames: createDisplayName(person),
             lastNames: [''],
             generation: person.generation,
             sex: person.sex ? person.sex : "M",
@@ -106,9 +131,13 @@ export default {
         id: person.id,
         name: person.name,
         title: person.titl,
-        firstNames: [createDisplayName(person)],
+        firstNames: createDisplayName(person),
         lastNames: [''],
         generation: person.generation,
+        birthplace: person.birthday_plac,
+        birthday: person.birth ?? formatDateToYMD(person.birthday),
+        deathday: person.death ?? formatDateToYMD(person.deathday),
+        deathplace: person.deathday_plac,
         sex: person.sex ? person.sex : "M",
         xref: 1,
         birth: getBirthYear(person),
@@ -158,6 +187,39 @@ export default {
   computed: {
     style() {
       return style
+    },
+    reportLevels() {
+      let reportLevels = {};
+
+      const traverse = (level, childLabel, person) => {
+        if (!person) return;
+        const parents = person.parents;
+        if (!parents) return;
+        if (!reportLevels[level]) reportLevels[level] = { level: level, data: {} };
+
+        parents.forEach(p => {
+          let label = Number(childLabel) * 2;
+          if (p.sex == 'F') label += 1;
+          reportLevels[level].data[label] = {
+            person: p,
+            name: p.firstNames,
+            label: label,
+          }
+          traverse(level + 1, label, p);
+        });
+      };
+
+      reportLevels[1] = { level: 1, data: {
+        1: {
+          person: this.familyData,
+          name: this.familyData.firstNames,
+          label: 1,
+        }
+      }};
+
+      traverse(2, 1, this.familyData);
+
+      return reportLevels;
     }
   }
 };
